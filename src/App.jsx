5cc40b58d15c6a5781038556f6127f8b744a07a7
Wessam Mounchit <misterw06@gmail.com>
2023-10-08 20:35:56 +0200

    Added pages and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import './App.css'
-import Navbar from './components/Navbar'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import Gallery from "./pages/Gallery"
+import Price from "./pages/Price"
+import Contact from "./pages/Contact"
+
 
 function App() {
-
   return (
-    <Navbar />
-  )
+    <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/gallerie" element={<Gallery />} />
+          <Route path="/tarifs" element={<Price />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
