889cca6ddf3213ee77e0457f094f7381ac99c206
Wessam Mounchit <misterw06@gmail.com>
2023-10-15 09:35:37 +0200

    Add Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,69 @@
-const Contact = () => {
-  return <div>Contact</div>;
-};
+import { useState } from "react";
 
-export default Contact;
+function ContactForm() {
+  const [formData, setFormData] = useState({
+    nom: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  return (
+    <>
+      <h1 className="font-secondary italic font-bold text-5xl text-center pt-14 text-white">
+        Contactez-moi
+      </h1>
+      <div className="p-8 rounded-lg shadow-md max-w-3xl mx-auto mt-10">
+        <form name="contact" method="POST" netlify>
+          <div className="mb-7">
+            <input
+              type="text"
+              id="nom"
+              name="nom"
+              value={formData.nom}
+              onChange={handleChange}
+              placeholder="Votre Nom"
+              className="w-full border border-gray-300 p-2 rounded-md focus:outline-none focus:border-primary"
+            />
+          </div>
+          <div className="mb-7">
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="Votre Adresse e-mail"
+              className="w-full border border-gray-300 p-2 rounded-md focus:outline-none focus:border-primary"
+            />
+          </div>
+          <div className="mb-7">
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              placeholder="Votre Message"
+              rows="4"
+              className="w-full border border-gray-300 p-2 rounded-md focus:outline-none focus:border-primary"
+            />
+          </div>
+          <div className="text-center">
+            <button
+              type="submit"
+              className="bg-primary hover:bg-secondary text-white w-full py-2 px-4 rounded-md"
+            >
+              Envoyer
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default ContactForm;
