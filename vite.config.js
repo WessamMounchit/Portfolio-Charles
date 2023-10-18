80238ed09058ad54984c4d5487d28691a0e834a3
Wessam Mounchit <misterw06@gmail.com>
2023-10-18 03:20:33 +0200

    Add vite pwa plugin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        short_name: "Portfolio Charles",
+        name: "Portfolio Charles Cantin",
+        description: "Découvrez l'univers d'un passioné de la photographie",
+        icons: [
+          {
+            src: "camera.svg",
+            sizes: "64x64 32x32 24x24 16x16",
+            type: "image/svg+xml",
+          },
+          {
+            src: "camera.svg",
+            type: "image/svg+xml",
+            sizes: "128x128",
+          },
+          {
+            src: "camera.svg",
+            type: "image/svg+xml",
+            sizes: "512x512",
+          },
+        ],
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#000000",
+        background_color: "#000000",
+      }
+    })
+  ],
 })
