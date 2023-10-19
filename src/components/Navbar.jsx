650052e579f51962851c46119b39e120be2964a5
Wessam Mounchit <misterw06@gmail.com>
2023-10-19 04:08:21 +0200

    fix problem in home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,8 +31,8 @@ const Nav = () => {
   ];
 
   return (
-    <nav className="shadow-md fixed w-full z-[9] top-0 left-0">
-      <div className=" items-center justify-between bg-black px-7 md:flex">
+    <nav className="fixed w-full z-[9] top-0 left-0">
+      <div className=" items-center justify-between px-7 md:flex">
         <div
           className="font-bold text-2xl flex items-center
         text-gray-800"
@@ -64,10 +64,10 @@ const Nav = () => {
         </div>
 
         <ul
-          className={`flex flex-col justify-center h-[110vh] absolute bg-black bg-opacity-80 left-0 w-full
+          className={`flex flex-col justify-center h-[110vh] absolute left-0 w-full
           -z-10 transition-all duration-300 ease-linear md:z-0 md:flex-row md:items-center md:pb-0
            md:static md:w-auto md:pl-0 md:h-auto ${
-             open ? "-top-10" : "top-[-740px]"
+             open ? "-top-10 bg-black opacity-80" : "top-[-740px]"
            }`}
         >
           {navigation.map((link) => (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -32,19 +32,20 @@ const Home = () => {
     content = (
       <>
         <div
-          className="h-[50%] md:h-full w-full bg-no-repeat bg-contain bg-center flex justify-center items-center text-center flex-col gap-4"
+          className="h-full w-full bg-no-repeat bg-contain bg-center flex justify-center items-center text-center flex-col gap-4"
           style={{
-            backgroundImage: `url(${errorImg})`,
+            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.65)),url(${errorImg})`,
           }}
-        ></div>
-        <div className="h-1/4 md:h-auto flex flex-col gap-3 sm:gap-7 justify-center items-center text-center pb-8 sm:p-0 sm:mr-8 sm:mt-6">
-          <h1 className="text-white font-medium italic font-primary text-xl xl:text-5xl">
-            Charles Cantin
-          </h1>
-          <p className="text-white font-medium text-lg mx-2 font-secondary xl:text-3xl">
-            Amateur de photographie passionné, je saisis l&apos;essence de vos
-            moments les plus précieux.
-          </p>
+        >
+          <div className="h-1/4 md:h-auto flex flex-col gap-3 sm:gap-7 justify-center items-center text-center pb-8 sm:p-0 sm:mr-8 sm:mt-6">
+            <h1 className="text-white font-medium italic font-primary text-xl xl:text-5xl">
+              Charles Cantin - Photographe
+            </h1>
+            <p className="text-white font-medium text-lg mx-2 font-secondary xl:text-3xl">
+              Amateur de photographie passionné, je saisis l&apos;essence de vos
+              moments les plus précieux.
+            </p>
+          </div>
         </div>
       </>
     );
@@ -53,18 +54,21 @@ const Home = () => {
     content = home.data?.map((home, index) => (
       <Fragment key={index}>
         <div
-          className="h-[50%] md:h-full w-full bg-no-repeat bg-contain bg-center flex justify-center items-center text-center flex-col gap-4"
+          className="h-full w-full bg-no-repeat bg-cover bg-center flex justify-center items-center text-center flex-col gap-4"
           style={{
-            backgroundImage: `url(${urlFor(home.imgUrl)})`,
+            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.65)), url(${urlFor(
+              home.imgUrl
+            )})`,
           }}
-        ></div>
-        <div className="h-1/4 md:h-auto flex flex-col gap-3 sm:gap-7 justify-center items-center text-center pb-8 sm:p-0 sm:mr-8 sm:mt-6">
-          <h1 className="text-white font-medium italic font-primary text-xl xl:text-5xl">
-            {home.title}
-          </h1>
-          <p className="text-white font-medium text-lg mx-2 font-secondary xl:text-3xl">
-            {home.description}
-          </p>
+        >
+          <div className="h-1/4 md:h-auto flex flex-col gap-3 sm:gap-7 justify-center items-center text-center pb-8 sm:p-0 sm:mr-8 sm:mt-6">
+            <h1 className="text-white font-medium italic font-primary text-xl xl:text-5xl">
+              {home.title}
+            </h1>
+            <p className="text-white font-medium text-lg mx-2 font-secondary xl:text-3xl">
+              {home.description}
+            </p>
+          </div>
         </div>
       </Fragment>
     ));
